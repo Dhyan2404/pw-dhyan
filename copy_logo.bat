93bf152a3a8560e744bb5b0d6922d16652ee4585
dharvip2112 <dharvip.2112@gmail.com>
2026-09-23 14:22:54 +0530

    feat: save official PW DHYAN logo into app drawable, mipmaps, and admin console

---
+++ copy_logo.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Copying PW DHYAN logo into Android app and Web Admin...
+copy /Y "c:\Users\dhyan\Downloads\Images\ChatGPT Image Sep 23, 2026, 02_16_04 PM.png" "d:\Dhyan\websites\pw-dhyan\app\src\main\res\drawable\app_logo.png"
+copy /Y "c:\Users\dhyan\Downloads\Images\ChatGPT Image Sep 23, 2026, 02_16_04 PM.png" "d:\Dhyan\websites\pw-dhyan\admin-manage\logo.png"
+echo Done copying logo files!
+pause
